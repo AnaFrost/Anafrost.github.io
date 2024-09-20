1ed577fd1f6593a12e8404dc31077fa893ea18cc
Anastasia Morozova <anafrost1993@gmail.com>
2024-09-20 19:49:43 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      <div className='wrapper'>
+        <h2 className='title'>ᛝ День рождения в берлоге эльфа ᛝ</h2>
+        <div className='dividing-line' />
+        <p className='description'>
+          Приглашаю вас на празднование моего 31 дня рождения 02.11 в 13:00.
+          <br />
+          Место проведения: Берлога эльфа
+          <br />
+          Место сбора: Культура с 12:20 до 12:40
+        </p>
+        <div className='dividing-line' />
+        <p className='description'>
+          Развлекательная программа: <br />
+          Будем делать свечи из вощины и в кокосовой скорлупе, каждая сможет
+          выбрать аромат и украшение свечи себе по душе
+          <br />
+          После мастер-класса будет торт и чай из алтайских трав, можно будет
+          сделать фотографии в лесной фотозоне
+        </p>
+        <p className='description'>
+          Место атмосферное и мне хочется поддержать настроение этой встречи
+          <br />
+          Буду рада если вы найдете в своем гардеробе вещи цветов:
+          <div className='color-ref'>
+            <div className='color-box gray' />
+            <div className='color-box brown' />
+            <div className='color-box green' />
+            <div className='color-box beige' />
+            <div className='color-box white' />
+          </div>
+          <br />
+          Точное совпадение не обязательно :)
+        </p>
+        <div className='dividing-line' />
+        <p className='description'>
+          Буду рада провести с вами этот день, до встречи и не забудте хорошее
+          настроение, будем колдунствовать✨
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
